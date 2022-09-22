659aa6dc71b5a1a06880db8dc877d353e3e69cd0
Kendell Andrews <kendellandrews2@gmail.com>
2022-09-22 14:35:58 -0700

    rollbar

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ app.use(cors())
 
 const students = ['Kendell', 'Amelia', 'Kelly']
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '6bb8371fdd2e419888aac57d931cd86f',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'))
 })
